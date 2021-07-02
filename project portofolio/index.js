fa57bc8d79404ba29ef019382ea6dbb70dcc2ee6
raviyaswanth <67175385+RAVIYASWANTHSAI@users.noreply.github.com>
2021-07-02 14:40:32 +0530

    new website

--- project portofolio/index.js
+++ project portofolio/index.js
@@ -1,6 +1,19 @@
 $(document).ready(function(){
+
 $("#burger").on("click",function(){
 $(".list").toggleClass("list-vis");
 });
+
+$(".first-second").hover(function(){
+$(".first-second").toggleClass("first-second-add");
+});
+
+var typed = new Typed(".typing",{
+strings : ["web developer", "content creator" ,"python developer"],
+typeSpeed : 100,
+backSpeed : 60,
+loop : true
+});
+
 
 });
