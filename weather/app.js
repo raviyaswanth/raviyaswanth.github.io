b42b600b880197920f4d56520edc8f184242317e
raviyaswanth <67175385+RAVIYASWANTHSAI@users.noreply.github.com>
2021-07-03 17:00:29 +0530

    made a complete weather application

--- weather/app.js
+++ weather/app.js
@@ -1,20 +1,45 @@
 $(document).ready(function(){
 let long;
 let lat;
-function getlocation(){
+var temp_c;
+let img_add;
+
 if(navigator.geolocation){
-navigator.geolocation.getCurrenPosition(showPosition);
-}
-else{
-$("body div").css("display","none");
-}
-}
-function showPosition(position){
+navigator.geolocation.getCurrentPosition(position =>{
 lat = position.coords.latitude;
 long = position.coords.longitude;
-console.log(lat);
+const key = "9733af41de3473cdfa81f8e83bed9cbe";
+const api = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${key}`;
+fetch(api)
+.then(response =>{
+return response.json();
+})
+.then(data =>{
+const{name} = data;
+const{temp} = data.main;
+const{icon,description} = data.weather[0];
+console.log(data);
+$(".location h1").text(name);
+$(".temperature h1").text(temp);
+$(".summary h1").text(description);
+$(".temperature").on("click",function(){
+if($(".temperature span").text()==="K"){
+temp_c = temp-273;
+temp_c = temp_c.toFixed(2);
+$(".temperature h1").text(temp_c);
+$(".temperature span").text("C");}
+else{
+$(".temperature h1").text(temp);
+$(".temperature span").text("K");
+}
+});
+
+img_add = icon+".png";
+$("img").attr("src",img_add);
+});
+
+});
+
 
 }
-
-
 });
